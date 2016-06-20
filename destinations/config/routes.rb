5ae93a225a1ccaebc212ecbfd934c9820707eda1
cfcrawford <camilafcrawford@gmail.com>
2016-06-20 15:15:18 -0700

    Modifies show route

--- destinations/config/routes.rb
+++ destinations/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   post "/users", to: "users#create"
 
-  get "/users/:id", to: "users#show"
+  get "/users/:id", to: "users#show", as: "user"
 
   get "/sign_in", to: "sessions#new"
 
