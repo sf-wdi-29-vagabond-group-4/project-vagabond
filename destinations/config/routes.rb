96472446bed850147fb8907f1692151420a3baa8
petenut <min.sb.kim@gmail.com>
2016-06-21 11:09:08 -0700

    no changes made

--- destinations/config/routes.rb
+++ destinations/config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show", as: "user"
   get "/sign_in", to: "sessions#new"
   post "/sessions", to: "sessions#create"
-
-
   delete "/sessions", to: "sessions#destroy", as: "delete"
-
-
   resources :venues
-
-
-
   get "/users/:id/edit", to: "users#edit"
-
   patch "/users/:id", to: "users#update"
-
-
 end
+
+
+
+
+
+
+
+
+
+
